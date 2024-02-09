e85a72eb0967027038724e36ea0b8a00a3629388
Mohamed Chaabi <145591877+mChaabi@users.noreply.github.com>
2024-02-09 21:15:17 +0100

    modified:   restaurant-frontend/src/component/Main.jsx

--- restaurant-frontend/src/component/Main.jsx
+++ restaurant-frontend/src/component/Main.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import Home from './Home'
 import Service from './Service'
 import About from './About'
+import Menu from './Menu'
+import Reservation from './Reservation'
+import Features from './Features'
+import SpecialDish from './SpecialDish'
+
 
 export default function Main() {
   return (
